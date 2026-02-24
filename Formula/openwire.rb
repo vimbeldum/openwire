820264079a6c86d91b79521d615e8c2cf4093a4a
shwetanshu21 <shwetanshu.mehta@gmail.com>
2026-02-25 00:39:23 +0530

    chore: bump version to 0.2.4 [skip ci]

--- Formula/openwire.rb
+++ Formula/openwire.rb
@@ -1,8 +1,8 @@
 class Openwire < Formula
   desc "P2P encrypted messenger for local networks with zero configuration"
   homepage "https://github.com/shwetanshu21/openwire"
-  url "https://github.com/shwetanshu21/openwire/archive/refs/tags/v0.2.3.tar.gz"
-  sha256 "c67a7890ae3eab0cc4cfa567ea8efd73717802d9e1ea5474ec82734788fd32d2"
+  url "https://github.com/shwetanshu21/openwire/archive/refs/tags/v0.2.4.tar.gz"
+  sha256 "f2a846f0eb3d82be91da9191a515005cbeb4b10b1dcb2a0ae81ab1bcffbc8bc3"
   license "MIT"
   head "https://github.com/shwetanshu21/openwire.git", branch: "master"
 
